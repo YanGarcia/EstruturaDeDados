9d5a3283b62ff30037cdb147defae5e5a641ac42
YanGarcia <65686063+YanGarcia@users.noreply.github.com>
2025-03-17 19:40:12 -0300

    Update PilhaExercício.java
    Agora a classe Pilha permite entrada do usuário usando a classe Scanner.

--- Pilhas/PilhaExercício.java
+++ Pilhas/PilhaExercício.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Pilha {
     private int topo;
     private int[] elementos;
@@ -42,6 +44,10 @@ class Pilha {
     }
 
     public void imprimirPilha() {
+        if (isEmpty()) {
+            System.out.println("Pilha vazia!");
+            return;
+        }
         System.out.print("Pilha: ");
         for (int i = 0; i <= topo; i++) {
             System.out.print(elementos[i] + " ");
@@ -52,13 +58,44 @@ class Pilha {
 
 public class TestePilha {
     public static void main(String[] args) {
-        Pilha pilha = new Pilha(5);
-        pilha.push(10);
-        pilha.push(20);
-        pilha.push(30);
-        pilha.imprimirPilha();
-        System.out.println("Topo da pilha: " + pilha.peek());
-        System.out.println("Removendo: " + pilha.pop());
-        pilha.imprimirPilha();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a capacidade da pilha:");
+        int capacidade = scanner.nextInt();
+        Pilha pilha = new Pilha(capacidade);
+
+        while (true) {
+            System.out.println("\nEscolha uma ação:");
+            System.out.println("1 - Empilhar elemento");
+            System.out.println("2 - Desempilhar elemento");
+            System.out.println("3 - Ver topo da pilha");
+            System.out.println("4 - Imprimir pilha");
+            System.out.println("5 - Sair");
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o elemento a ser empilhado:");
+                    int elemento = scanner.nextInt();
+                    pilha.push(elemento);
+                    break;
+                case 2:
+                    System.out.println("Elemento desempilhado: " + pilha.pop());
+                    break;
+                case 3:
+                    System.out.println("Topo da pilha: " + pilha.peek());
+                    break;
+                case 4:
+                    pilha.imprimirPilha();
+                    break;
+                case 5:
+                    System.out.println("Saindo...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
     }
 }
